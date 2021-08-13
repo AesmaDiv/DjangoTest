228c51e645672b2bc603b653dd0b15a94ca62de6
AesmaDiv <jhwh.0001@gmail.com>
2021-08-13 14:58:26 +0500

    Adding git_update

--- store_api/filters.py
+++ store_api/filters.py
@@ -7,13 +7,10 @@ class CategoryFilter(filters.BaseInFilter, filters.CharFilter):
 
 
 class ProductFilterSet(filters.FilterSet):
-    # id = filters.Filter()
     name = filters.CharFilter(lookup_expr='contains')
     category_id = CategoryFilter(field_name='categories__id', lookup_expr='in')
     category_name = CategoryFilter(field_name='categories__name', lookup_expr='in')
     price = filters.Filter()
-    # posted = filters.Filter()
-    # deleted = filters.Filter()
 
     class Meta:
         model = Product
